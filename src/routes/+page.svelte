6f44f2166e46dd7a4b20470fdc4ae6478bed6eba
David Smith <dasmith@zero-to-prod.com>
2022-12-25 21:53:51 -0500

    Add apollo client

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    import { query } from "svelte-apollo";
+    import {gql, InMemoryCache} from "@apollo/client/core";
+    import { ApolloClient } from "@apollo/client/core";
+    import { setClient } from "svelte-apollo";
+    const client = new ApolloClient({
+        uri: "http://localhost:89/graphql",
+        cache: new InMemoryCache()
+    });
+    setClient(client);
+
+    const USER = gql`
+        query {
+              user(id: 1) {
+                id
+                name
+                email
+                created_at
+              }
+        }
+`;
+
+
+    const user = query(USER);
+    console.log(user.refetch());
+</script>
