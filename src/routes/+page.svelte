d5fd7882cec8b7d2132fd503db6b026ee93c89cc
David Smith <dasmith@zero-to-prod.com>
2023-01-01 18:22:55 -0500

    Add animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,14 @@
 	import UeCreate from '$lib/api/UeCreate';
 	import type { Ue } from '$lib/types';
 	import { page } from '$app/stores';
+	import { slide } from 'svelte/transition';
 
 	let name: Ue['name'];
 	let date: string | undefined = undefined;
 	let time: string | undefined = undefined;
 	let location: Ue['location'];
 	let description: Ue['description'];
-	let ue: Ue;
+	let ue: Ue | undefined = undefined;
 
 	$: required = !!time;
 
@@ -22,11 +23,20 @@
 	};
 
 	async function onSubmit() {
-		ue = await UeCreate({ name, time: combined_time(), location, description });
+		const data = { name, time: combined_time(), location, description };
+		if (!ue) {
+			ue = await UeCreate(data);
+		} else {
+			ue = undefined;
+			setTimeout(async () => {
+				ue = await UeCreate(data);
+			}, 500);
+		}
 	}
 	const select = (event: MouseEvent & { currentTarget: EventTarget & HTMLInputElement }) =>
 		event.currentTarget.select();
 	const share = async () => {
+		if (!ue) return;
 		try {
 			const title = `${ue.name} ${ue.time ? new Date(ue.time).toDateString() : ''}`;
 			const description = ue.description ? '| ' + ue.description : '';
@@ -43,73 +53,75 @@
 </script>
 
 <div class="bg-gradient-to-r from-violet-500 to-fuchsia-500 py-10">
-	<div class="m-4 gap-4 shadow rounded-2xl bg-gray-800 p-4 md:w-1/2 lg:w-1/3">
-		<h1 class="text-center text-2xl text-white">Create an Event</h1>
-		<form on:submit|preventDefault={onSubmit} class="flex flex-col ">
-			<label>
-				<span class="label">Name</span>
-				<input
-					bind:value={name}
-					name="name"
-					minlength="2"
-					maxlength="250"
-					placeholder="Event name"
-					required
-					autocomplete="off"
-				/>
-			</label>
-			<div class="flex flex-col gap-4 xs:flex-row">
-				<label class="w-full">
-					<span class="label">Date</span>
+	<div class="m-4 shadow rounded-2xl bg-gray-800 p-4 md:w-1/2 lg:w-1/3 divide-y">
+		<div>
+			<h1 class="text-center text-2xl text-white">Create an Event</h1>
+			<form on:submit|preventDefault={onSubmit} class="flex flex-col ">
+				<label>
+					<span class="label">Name</span>
 					<input
-						bind:value={date}
-						name="date"
-						min={new Date().toISOString().split('T')[0]}
-						type="date"
-						{required}
+						bind:value={name}
+						name="name"
+						minlength="2"
+						maxlength="250"
+						placeholder="Event name"
+						required
+						autocomplete="off"
 					/>
 				</label>
-				<label class="w-full">
-					<span class="label">Time</span>
-					<input bind:value={time} name="time" type="time" />
+				<div class="flex flex-col gap-4 xs:flex-row">
+					<label class="w-full">
+						<span class="label">Date</span>
+						<input
+							bind:value={date}
+							name="date"
+							min={new Date().toISOString().split('T')[0]}
+							type="date"
+							{required}
+						/>
+					</label>
+					<label class="w-full">
+						<span class="label">Time</span>
+						<input bind:value={time} name="time" type="time" />
+					</label>
+				</div>
+
+				<label>
+					<span class="label">Description</span>
+					<textarea
+						bind:value={description}
+						name="description"
+						placeholder="Add a description"
+						maxlength="250"
+					/>
 				</label>
-			</div>
 
-			<label>
-				<span class="label">Description</span>
-				<textarea
-					bind:value={description}
-					name="description"
-					placeholder="Add a description"
-					maxlength="250"
-				/>
-			</label>
-
-			<button type="submit" class="mt-4 create-link"
-				><span
-					><svg
-						class="h-4 mr-1.5 fill-gray-800"
-						focusable="false"
-						aria-hidden="true"
-						viewBox="2 5 20 12"
-						><path
-							d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"
-						/></svg
-					></span
-				>Get Link</button
-			>
-		</form>
+				<button type="submit" class="mt-4 create-link"
+					><span
+						><svg
+							class="h-4 mr-1.5 fill-gray-800"
+							focusable="false"
+							aria-hidden="true"
+							viewBox="2 5 20 12"
+							><path
+								d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"
+							/></svg
+						></span
+					>Get Link</button
+				>
+			</form>
+		</div>
 		{#if ue}
-			<div class="flex gap-4 rounded">
+			<div class="flex gap-4 pt-2 border-gray-600 mt-6" transition:slide>
 				<label class="w-full text-white">
 					Shareable Link
-					<input class="bg-gray-300 text-sm" on:click={select} value={`${$page.url}${ue.id}`} />
+					<input class="bg-gray-300 text-xs" on:click={select} value={`${$page.url}${ue.id}`} />
 				</label>
 				<div class="mt-auto">
 					<button type="submit" class="share-link" on:click={share}
 						><span
 							><svg
-								class="h-4 mr-1.5 fill-gray-800"
+								class="h-4 mr-2 fill-gray-800"
 								focusable="false"
 								aria-hidden="true"
 								viewBox="0 0 22 22"
