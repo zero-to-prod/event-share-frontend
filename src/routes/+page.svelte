1e4e306cc36f799759c255c92d641c7581497b7e
David Smith <dasmith@zero-to-prod.com>
2023-01-01 14:08:41 -0500

    remove flip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,28 @@
 	import { page } from '$app/stores';
 
 	let name: Ue['name'];
-	let time: Ue['time'] = new Date().toISOString().split('T')[0]; // 2005-06-07
+	let date: string | undefined = undefined;
+	let time: string | undefined = undefined;
 	let location: Ue['location'];
 	let description: Ue['description'];
 	let data: Ue;
 
+	$: required = !!time;
+
+	let combined = () => {
+		if (date && time) {
+			return `${date}T${time}`;
+		}
+		if (date) {
+			return date;
+		}
+	};
+
 	async function onSubmit() {
-		data = await UeCreate({ name, time, location, description });
+		data = await UeCreate({ name, time: combined(), location, description });
 	}
+	const select = (event: MouseEvent & { currentTarget: EventTarget & HTMLInputElement }) =>
+		event.currentTarget.select();
 	const share = async () => {
 		try {
 			// noinspection TypeScriptValidateTypes
@@ -27,62 +41,86 @@
 </script>
 
 <div class="py-10 bg-gradient-to-r from-violet-500 to-fuchsia-500">
-	<div class="m-auto grid m-4 p-4 bg-gray-800 sm:w-1/3 rounded-md">
-		<h1 class="text-white text-2xl mb-4">Create an Event</h1>
-		<form on:submit|preventDefault={onSubmit} class="grid gap-4">
+	<div class="gap-4 m-4 p-4 bg-gray-800 lg:w-1/3 md:w-1/2 rounded-md">
+		<h1 class="text-white text-2xl text-center">Create an Event</h1>
+		<form on:submit|preventDefault={onSubmit} class="flex flex-col space-y-4">
 			<label>
-				<input bind:value={name} name="name" placeholder="Event name" />
+				<span class="text-white text-lg ml-1">Name</span>
+				<input
+					bind:value={name}
+					name="name"
+					minlength="2"
+					maxlength="250"
+					placeholder="Event name"
+					required
+					autocomplete="off"
+				/>
 			</label>
-			<label>
-				<input bind:value={time} name="time" type="date" />
-			</label>
-			<label>
-				<textarea bind:value={description} name="description" placeholder="Add a description" />
-			</label>
-			<div class="flex flex-row-reverse">
-				<button type="submit" class="create-link"
-					><span
-						><svg
-							class="h-4 mr-1.5 fill-gray-800"
-							focusable="false"
-							aria-hidden="true"
-							viewBox="2 5 20 12"
-							><path
-								d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"
-							/></svg
-						></span
-					>Get Link</button
-				>
+			<div class="flex flex-col xs:flex-row gap-4">
+				<label class="w-full ">
+					<span class="text-white text-lg ml-1">Date</span>
+					<input
+						bind:value={date}
+						name="date"
+						min={new Date().toISOString().split('T')[0]}
+						type="date"
+						{required}
+					/>
+				</label>
+				<label class="w-full">
+					<span class="text-white text-lg ml-1">Time</span>
+					<input bind:value={time} name="time" type="time" />
+				</label>
 			</div>
-		</form>
-	</div>
 
-	{#if data}
-		<div class="grid gap-2">
-			<button on:click={share}> Share </button>
-			<a data-sveltekit-preload-data="hover" href={`/${data.id}`}>View Event</a>
-			<div class="flex">
-				<div class="flex-1 rounded bg-gray-100">
-					<label class="grid text-sm">
-						<span class="pt-1 pl-1"> Link to event </span>
-						<input
-							value={`${$page.url}${data.id}`}
-							class="border-b-2 bg-gray-100 p-1 focus:border-b-2 focus:border-blue-500 focus:outline-0"
-						/>
-					</label>
-				</div>
-				<div class="">
-					<div class="flex p-3 align-middle">
-						<button
-							class="items-center rounded-md bg-white px-3 py-2 text-sm font-medium leading-4 text-blue-500 hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-						>
-							Copy</button
-						>
-					</div>
+			<label>
+				<span class="text-white text-lg ml-1">Description</span>
+				<textarea
+					bind:value={description}
+					name="description"
+					placeholder="Add a description"
+					maxlength="250"
+				/>
+			</label>
+
+			<button type="submit" class="create-link mt-2"
+				><span
+					><svg
+						class="h-4 mr-1.5 fill-gray-800"
+						focusable="false"
+						aria-hidden="true"
+						viewBox="2 5 20 12"
+						><path
+							d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"
+						/></svg
+					></span
+				>Get Link</button
+			>
+		</form>
+		{#if data}
+			<div class="rounded flex gap-4">
+				<label class="text-white w-full">
+					Shareable Link
+					<input class="text-sm bg-gray-300" on:click={select} value={`${$page.url}${data.id}`} />
+				</label>
+				<div class="mt-auto">
+					<button type="submit" class="share-link" on:click={share}
+						><span
+							><svg
+								class="h-4 mr-1.5 fill-gray-800"
+								focusable="false"
+								aria-hidden="true"
+								viewBox="0 0 22 22"
+								><path
+									d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"
+								/></svg
+							></span
+						>Share</button
+					>
 				</div>
 			</div>
-		</div>
-	{/if}
+		{/if}
+	</div>
 </div>
 
 <style lang="postcss">
@@ -91,9 +129,12 @@
 	}
 	input,
 	textarea {
-		@apply block w-full rounded-md p-2 text-lg text-gray-700 focus:border-fuchsia-500 focus:outline-0 focus:ring-2 focus:ring-fuchsia-500 focus:ring-offset-gray-800 focus:ring-offset-2;
+		@apply mt-1 block w-full rounded-md p-2 text-lg text-gray-700 focus:border-fuchsia-500 focus:outline-0 focus:ring-2 focus:ring-fuchsia-500 focus:ring-offset-gray-800 focus:ring-offset-2;
 	}
 	.create-link {
-		@apply inline-flex rounded-md items-center border border-transparent bg-fuchsia-600 px-4 py-3.5 font-medium leading-4 text-white shadow-sm hover:bg-fuchsia-700 focus:outline-none focus:ring-2 focus:ring-fuchsia-500 focus:ring-offset-gray-800 focus:ring-offset-2;
+		@apply justify-center inline-flex rounded-md items-center border border-transparent bg-fuchsia-600 px-4 py-3.5 font-medium leading-4 text-white shadow-sm hover:bg-fuchsia-700 focus:outline-none focus:ring-2 focus:ring-fuchsia-500 focus:ring-offset-gray-800 focus:ring-offset-2;
+	}
+	.share-link {
+		@apply inline-flex rounded-md items-center border border-transparent bg-blue-600 px-4 py-3.5 font-medium leading-4 text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-gray-800 focus:ring-offset-2;
 	}
 </style>
