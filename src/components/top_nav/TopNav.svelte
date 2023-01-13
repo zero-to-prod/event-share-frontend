1ed6d253aa213352a5749f2ca89d68a07a9d426f
David Smith <dasmith@zero-to-prod.com>
2023-01-13 15:09:49 -0500

    Styling

--- src/components/top_nav/TopNav.svelte
+++ src/components/top_nav/TopNav.svelte
@@ -1,5 +1,5 @@
 <nav class="bg-gray-800">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+  <div class="mx-auto max-w-7xl px-2">
     <div class="relative flex h-16 items-center justify-between">
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex flex-shrink-0 items-center">
